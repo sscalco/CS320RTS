de8f85828f63ecd05c89ed49a0bd4947dae1b25a
sscalco <sscalco@ycp.edu>
2014-03-26 13:04:37 -0400

    added Garrison class

---
+++ RTS/src/edu/ycp/cs320/rts/shared/Garrison.java
@@ -0,0 +1,68 @@
+package edu.ycp.cs320.rts.shared;
+
+import java.util.ArrayList;
+
+public class Garrison extends Structure implements CanTransport{
+	int carryingCapacity;
+	ArrayList<Unit> troops;
+	/**
+	 * 
+	 */
+	public Garrison(){
+		super();
+		this.carryingCapacity = 0;
+		this.troops = new ArrayList<Unit>();
+	}
+	
+	public Garrison(int carryingCapacity)
+	/**
+	 * 
+	 */
+
+	public void setCarryingCapacity(int carryingCapacity) {
+		this.carryingCapacity = carryingCapacity;
+		return;
+	}
+	/**
+	 * 
+	 */
+
+	public int getCarryingCapacity() {
+		return carryingCapacity;
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public ArrayList<Unit> getPassenger() {
+		// TODO Auto-generated method stub
+		//Not sure what needs to go here.
+		return null;
+	}
+	/**
+	 * 
+	 */
+	@Override
+	public void load(Unit unit) {
+		troops.add(unit);
+		return;		
+	}
+	/**
+	 * 
+	 */
+	@Override
+	public void unload(ArrayList<Unit> unit) {
+		// TODO Auto-generated method stub
+		
+	}
+	/**
+	 * 
+	 */
+	@Override
+	public void unloadAll() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
