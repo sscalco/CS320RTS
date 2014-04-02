28fd1cb37b24ba6178ae98273ab9a71ea4efd143
Dan Mashuda <dmashuda@ycp.edu>
2014-04-02 13:52:53 -0400

    Cleaned out Default Project
    Removed GWT default project code from RTS.java
    Removed GWT default project code from RTS.html

--- RTS/src/edu/ycp/cs320/rts/client/RTS.java
+++ RTS/src/edu/ycp/cs320/rts/client/RTS.java
@@ -30,12 +30,12 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class RTS implements EntryPoint {
-	
+
 	private Image combatantSprite;
 	private Image unitSprite;
 	private Image structureSprite;
 	private Image turretSprite;
-	
+
 	/**
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
@@ -45,7 +45,8 @@ public class RTS implements EntryPoint {
 			+ "connection and try again.";
 
 	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
+	 * Create a remote service proxy to talk to the server-side Greeting
+	 * service.
 	 */
 	private final GreetingServiceAsync greetingService = GWT
 			.create(GreetingService.class);
@@ -54,158 +55,52 @@ public class RTS implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		
-		//load sprites
-				String combatantSpriteUrl = GWT.getModuleBaseForStaticFiles() + "combatantSprite.png";
-				String unitSpriteUrl = GWT.getModuleBaseForStaticFiles() + "unitSprite.png";
-				String structureSpriteUrl = GWT.getModuleBaseForStaticFiles() + "structureSprite.png";
-				String turretSpriteUrl = GWT.getModuleBaseForStaticFiles() + "turretSprite.png";
-				GWT.log("Combatant sprite: " + combatantSpriteUrl);
-				GWT.log("Unit sprite: " + unitSpriteUrl);
-				GWT.log("Structure sprite: " + structureSpriteUrl);
-				GWT.log("Turret sprite: " + turretSprite);
-				combatantSprite = new Image(combatantSpriteUrl);
-				unitSprite = new Image(unitSpriteUrl);
-				structureSprite = new Image(structureSpriteUrl);
-				turretSprite = new Image(turretSpriteUrl);
-				
-				//Generate a new game view
-				GameView view = new GameView();
-				ArrayList<GameObject> arr = new ArrayList<GameObject>();
-				Structure test = new Structure(1, 1, new Point(320, 240), new Point(128,128), 1, 100);
-				test.setImageName("structureSprite.png");
-				arr.add(test);
-				
-				view.setGameList(arr);					
-				
-				//more stuff
-				FlowPanel imagePanel = new FlowPanel();
-				imagePanel.add(combatantSprite);
-				imagePanel.add(unitSprite);
-				imagePanel.add(structureSprite);
-				imagePanel.add(turretSprite);
-				imagePanel.setSize("0px", "0px");
-				RootLayoutPanel.get().add(imagePanel);
-				
-				//add the view
-				RootLayoutPanel.get().add(view);
-				
-				view.setUnitSprite(unitSprite);
-				view.setCombatantSprite(combatantSprite);
-				view.setStructureSprite(structureSprite);
-				view.setTurretSprite(turretSprite);
-				
 
-				view.activate();
-		
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		// load sprites
+		String combatantSpriteUrl = GWT.getModuleBaseForStaticFiles()
+				+ "combatantSprite.png";
+		String unitSpriteUrl = GWT.getModuleBaseForStaticFiles()
+				+ "unitSprite.png";
+		String structureSpriteUrl = GWT.getModuleBaseForStaticFiles()
+				+ "structureSprite.png";
+		String turretSpriteUrl = GWT.getModuleBaseForStaticFiles()
+				+ "turretSprite.png";
+		GWT.log("Combatant sprite: " + combatantSpriteUrl);
+		GWT.log("Unit sprite: " + unitSpriteUrl);
+		GWT.log("Structure sprite: " + structureSpriteUrl);
+		GWT.log("Turret sprite: " + turretSprite);
+		combatantSprite = new Image(combatantSpriteUrl);
+		unitSprite = new Image(unitSpriteUrl);
+		structureSprite = new Image(structureSpriteUrl);
+		turretSprite = new Image(turretSpriteUrl);
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+		// Generate a new game view
+		GameView view = new GameView();
+		ArrayList<GameObject> arr = new ArrayList<GameObject>();
+		Structure test = new Structure(1, 1, new Point(320, 240), new Point(
+				128, 128), 1, 100);
+		test.setImageName("structureSprite.png");
+		arr.add(test);
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
+		view.setGameList(arr);
 
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
+		// more stuff
+		FlowPanel imagePanel = new FlowPanel();
+		imagePanel.add(combatantSprite);
+		imagePanel.add(unitSprite);
+		imagePanel.add(structureSprite);
+		imagePanel.add(turretSprite);
+		imagePanel.setSize("0px", "0px");
+		RootLayoutPanel.get().add(imagePanel);
 
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
+		// add the view
+		RootLayoutPanel.get().add(view);
 
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
+		view.setUnitSprite(unitSprite);
+		view.setCombatantSprite(combatantSprite);
+		view.setStructureSprite(structureSprite);
+		view.setTurretSprite(turretSprite);
 
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		view.activate();
 	}
 }
